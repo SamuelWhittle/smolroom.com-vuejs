cdaac2f6d8174ef9b32fa7162974022656dfbf0e
SamuelWhittle <swbnp7@gmail.com>
2022-11-30 08:55:57 -0600

    changed css for conways to be canvas-and-controls

---
+++ public/workers/MazesManager.js
@@ -0,0 +1,67 @@
+
+let canvas, ctx;
+let cDiv;
+
+let interval;
+
+self.onmessage = event => {
+  switch (event.data.msgType) {
+    case "toggleTask":
+      //toggleTask(event.data.state, event.data.fps);
+      step();
+      break;
+    case 'resizeCanvas':
+      canvas.width = event.data.width;
+      canvas.height = event.data.height;
+      //TODO?
+      break;
+    case 'task':
+      canvas = event.data.canvas;
+      cDiv = event.data.cDiv;
+
+      ctx = canvas.getContext('2d');
+      ctx.imageSmoothingEnabled = false;
+      ctx.lineWidth = 1;
+
+      //TODO
+
+      postMessage({ msgType: "ready" });
+      break;
+    case "newMaze":
+      //TODO
+      break;
+    case "clearPath":
+      //TODO
+      break;
+    case "terminate":
+      //console.log("waiter terminated");
+      self.close();
+      break;
+  }
+};
+
+postMessage({ msgType: "clockIn" });
+
+function step() {
+
+}
+
+function toggleTask(state, fps) {
+  if (state) {
+    setTimeInterval(fps);
+  } else {
+    clearTimeInterval();
+  }
+}
+
+function setTimeInterval(fps) {
+  clearTimeInterval();
+
+  interval = setInterval(step, 1000 / fps);
+}
+
+function clearTimeInterval() {
+  if (interval) {
+    clearInterval(interval);
+  }
+}
